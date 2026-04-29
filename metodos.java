9c00ead6a6b8aef54d2a447aca733dfa8caea67e
Saedi huiza flores <saedihuiza22@gmail.com>
2026-04-29 20:07:55 -0300

    Add files via upload

---
+++ metodos.java
@@ -0,0 +1,36 @@
+package manipulacao.de.arquivo;
+
+
+import javax.swing.JOptionPane;
+public class metodos {
+    
+    
+     
+  public Aluno[] FCadastra(Aluno[] aluno){
+  int i;
+
+  for (i = 0 ; i < 3 ; i++)
+   {
+    aluno[i].pnome = JOptionPane.showInputDialog("Entre o nome do aluno:");
+    aluno[i].unome = JOptionPane.showInputDialog("Entre o sobrenome do aluno:");
+    aluno[i].pontos =Integer.parseInt(JOptionPane.showInputDialog("Pontos do aluno:"));
+   }
+   
+  return aluno; } 
+   
+// PROCEDIMENTO RECEBENDO REGISTRO aluno TIPO ALUNO[]    
+  public void FConsulta(Aluno[] aluno){
+     int i;
+
+    for (i = 0 ; i < 3; i++) 
+       {
+         System.out.println(aluno[i].pnome + " " + aluno[i].unome  + " pontos: " + aluno[i].pontos);
+       }
+   }             
+
+ } 
+    
+
+    
+    
+
